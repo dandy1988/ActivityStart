57823bed6ed8f058bcc50bd3b2f6b26329963076
Antoninadan <mail100@i.ua>
2020-01-23 17:31:01 +0200

    initial commit

---
+++ app/src/main/java/com/dandy1988/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dandy1988;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
